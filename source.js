50fe207be5ad39d18176538cca8b0204ee0b53d8
Martin <marvahus@proton.me>
2023-11-08 21:21:28 +0100

    Made some improvements.

---
+++ source.js
@@ -0,0 +1,63 @@
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function createBingoCard() {
+    const bingoCard = document.getElementById("bingo-card");
+    const phrases = [
+      "Pirate Software Story",
+      "DnD chest",
+      "He who has no life",
+      "Talks about his hair",
+      "Psychoholics",
+      "Defcon Black Badge",
+      "AI Thor Error",
+      "Noods Folder",
+      "Asmondgold Clip",
+      "Work on game",
+      "ADs",
+      "Holding Ferret",
+      "Heidi Klum",
+      "Don't buy my game",
+      "Life Story",
+      "Shorts Strategy",
+      "Subscriber Count",
+      "No sevens in chat",
+      "Block Game",
+      "Hotdog Clip",
+      "I hacked powerplants",
+      "DJ Clancy is a Chad",
+      "Cursed Quest",
+      "Thor Old",
+      "Eggs Over MyHammy",
+      "I'm not Markiplier",
+      "Heartbound on Fridge",
+      "Sound like Howard Stern",
+      "Thor's out of touch",
+      "Our Dad",
+      "Chat is Goblins",
+      "Switch Box",
+      "Onion",
+    ];
+
+    shuffleArray(phrases);
+
+    let cellIndex = 0;
+    for (let row = 0; row < 5; row++) {
+        const tr = document.createElement('tr');
+
+        for (let col = 0; col < 5; col++) {
+            const td = document.createElement('td');
+            td.textContent = phrases[cellIndex];
+            tr.appendChild(td);
+            cellIndex++;
+        }
+
+        bingoCard.appendChild(tr);
+    }
+}
+
+createBingoCard();
